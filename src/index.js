7ec32803a8d30ddd23844241a01c3f0fbaa3015b
Tassos Lambrou <lambrou@gmail.com>
2018-02-24 22:06:16 -0800

    added server and completed the getting started

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(<ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, document.getElementById('root'));
+  
 registerServiceWorker();
